8d6ae02c4daa8aa05c776d0bb78a936678b8c2f1
Natassia Caldwell <natassia.caldwell@gmail.com>
2022-08-06 14:48:23 -0400

    token and itoken service added

--- ElevenNote.Services/User/IUserService.cs
+++ ElevenNote.Services/User/IUserService.cs
@@ -10,5 +10,6 @@ namespace ElevenNote.Services.User
     public interface IUserService
     {
         Task<bool> RegisterUserAsync(UserRegister model);
+        Task<UserDetail> GetUserByIdAsync(int userId);
     }
 }
